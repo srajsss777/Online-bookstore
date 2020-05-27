f31a504a18d7d0c11fbbccb391544565e7ba95ed
srajsss777 <srajsss777@gmail.com>
2020-05-28 05:05:01 +0530

    Initial commit, created created spring boot project and configure application.properties

---
+++ Onlinestoretest/src/main/java/com/alphacommunity/Onlinestoretest/OnlinestoretestApplication.java
@@ -0,0 +1,13 @@
+package com.alphacommunity.Onlinestoretest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinestoretestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinestoretestApplication.class, args);
+	}
+
+}
---
+++ Onlinestoretest/src/test/java/com/alphacommunity/Onlinestoretest/OnlinestoretestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphacommunity.Onlinestoretest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinestoretestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
